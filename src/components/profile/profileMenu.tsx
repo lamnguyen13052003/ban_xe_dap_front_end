f07e7c061324c7c4bfe645fa34416babfe0c4992
Nguyễn Đình Lam <21130416@st.hcmuaf.edu.vn>
2024-07-16 10:27:41 +0700

    get bills

--- src/components/profile/profileMenu.tsx
+++ src/components/profile/profileMenu.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import {Card, Button} from 'react-bootstrap';
 import Avatar from "../../assets/images/profile/me.png";
 import AvatarBackGround from "../../assets/images/profile/back-ground.png";
+import {MenuItem} from "../../pages/Profile";
 
-function ProfileMenu() {
+
+function ProfileMenu(props: { menuItemSelected: MenuItem, onSelected: (key: MenuItem) => void }) {
+
+
+    const getFormatSelectMenu = (key: MenuItem) => {
+        return key === props.menuItemSelected ? "text-white bg-primary" : "text-black bg-white";
+    }
+
     return (
         <Card border="light" className="text-center p-0 mb-4">
             <div
@@ -17,6 +25,28 @@ function ProfileMenu() {
                     className="user-avatar large-avatar rounded-circle mx-auto mt-n7 mb-4"
                 />
             </Card.Body>
+            <ul className={"list-unstyled border border-1 rounded-3 overflow-hidden"}>
+                <li className={`text-start  p-2 fw-bold ${getFormatSelectMenu("")}`}
+                    onClick={() => {
+                        props.onSelected("")
+                    }}
+                    style={{
+                        fontSize: "1.2rem"
+                    }}
+                >
+                    Thông tin cá nhân
+                </li>
+                <li className={`text-start p-2 fw-bold ${getFormatSelectMenu("BILL")}`}
+                    onClick={() => {
+                        props.onSelected("BILL")
+                    }}
+                    style={{
+                        fontSize: "1.2rem"
+                    }}
+                >
+                    Danh sách hóa đơn
+                </li>
+            </ul>
         </Card>
     );
 };
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,21 +1,95 @@
-import React from "react";
-import {Col, Container, Row} from 'react-bootstrap';
+import React, {useEffect, useState} from "react";
+import {Card, Col, Container, Row} from 'react-bootstrap';
 import ProfileMenu from "../components/profile/profileMenu";
 import ProfileForm from "../components/profile/personalInfo";
+import {InfoPayType} from "../types/infoPay.type";
+import axiosHttp from "../utils/axiosHttp";
+import {useSelector} from "react-redux";
+import {RootState} from "../configs/store";
+import {useNavigate} from "react-router-dom";
+import {AxiosResponse} from "axios";
+import {ResponseApi} from "../types/response.type";
+
+export type MenuItem = "" | "BILL"
 
 const ProfilePage: React.FC = () => {
+    const [menuSelected, setMenuSelected] = useState<MenuItem>("" as MenuItem)
+
+    const render = () => {
+        switch (menuSelected) {
+            case "":
+                return <ProfileForm/>
+            case "BILL":
+                return <BillManager/>
+        }
+    }
+
     return (
         <Container>
             <Row className={"justify-content-between"}>
                 <Col sm={3}>
-                    <ProfileMenu/>
+                    <ProfileMenu menuItemSelected={menuSelected} onSelected={(key) => {
+                        setMenuSelected(key)
+                    }}/>
                 </Col>
                 <Col sm={9}>
-                    <ProfileForm/>
+                    {render()}
                 </Col>
             </Row>
         </Container>
     );
 };
+
+function BillManager() {
+    const [bills, setBills] = useState<InfoPayType[]>([])
+    const auth = useSelector((state: RootState) => state.auth.user)
+    const nav = useNavigate();
+
+    useEffect(() => {
+        if (!auth || !auth._id) {
+            nav("/")
+            return;
+        }
+
+        if (bills.length) return;
+        axiosHttp.get<any, AxiosResponse<ResponseApi<InfoPayType[]>>, any>("/api/bill/{auth._id}")
+            .then((response) => {
+                const data = response.data.data;
+                if (!data) setBills([])
+                else setBills(data)
+            })
+    }, [])
+
+    return (
+        <Card border="light" className="bg-white shadow-sm mb-4 p-3">
+            <h4>Quản lý hóa đơn</h4>
+            <Row className={"fs-5 mt-3"}>
+                <Col xs={1}>STT</Col>
+                <Col xs={2}>Ho và tên</Col>
+                <Col xs={2}>SĐT</Col>
+                <Col xs={5}>Địa chỉ</Col>
+                <Col xs={2} className={"text-center"}>Phương thức thanh toán</Col>
+            </Row>
+            {
+                bills.map((value, index) =>
+                    <BillItem data={value} index={index} key={index}/>
+                )
+            }
+        </Card>
+    )
+}
+
+function BillItem(props: { index: number, data: InfoPayType }) {
+    const {fullName, email, phoneNumber, fullAddress, payMethod} = props.data
+    return (
+        <Row>
+            <Col xs={1}>{props.index}</Col>
+            <Col xs={2}>{fullName}</Col>
+            <Col xs={2}>{email}</Col>
+            <Col xs={5}>{fullAddress}</Col>
+            <Col xs={2} className={"text-center"}>{payMethod}</Col>
+        </Row>
+    );
+}
 
 export default ProfilePage;
